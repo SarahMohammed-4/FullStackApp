af8a143cd1f29c18d14a4876c2c6daa1c66cc979
SarahMohammed-4 <sarah.mohammed.zzr@gmail.com>
2025-10-05 04:17:23 +0300

    Update customer.service.ts

--- frontend/src/app/customer.service.ts
+++ frontend/src/app/customer.service.ts
@@ -14,7 +14,7 @@ export interface Customer {
 
 export class CustomerService {
 
-  private apiUrl = "/customers";
+  private apiUrl = "http://52.58.195.115:30101/customers";
 
   constructor(private http: HttpClient) {}
 
